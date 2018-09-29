e444c32f736b4fb5e1b56cd8ab3f136277da1132
鲁龙基 <lulj@yuntongxun.com>
2018-09-29 11:05:36 +0800

    初始化

--- wx-web-rest/src/main/java/com/yuntongxun/config/RedisConfig.java
+++ wx-web-rest/src/main/java/com/yuntongxun/config/RedisConfig.java
@@ -26,10 +26,13 @@ public class RedisConfig {
     @Bean
     @ConditionalOnMissingBean(name = "redisTemplate")
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();         //使用fastjson序列化
-        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);        // value值的序列化采用fastJsonRedisSerializer
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        //使用fastjson序列化
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
         template.setValueSerializer(fastJsonRedisSerializer);
-        template.setHashValueSerializer(fastJsonRedisSerializer);        // key的序列化采用StringRedisSerializer
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        // key的序列化采用StringRedisSerializer
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setConnectionFactory(redisConnectionFactory);
