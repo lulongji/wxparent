27cbc38cddb432a72eb99dac7116ef36997c9311
鲁龙基 <lulj@yuntongxun.com>
2018-10-10 09:23:05 +0800

    初始化

--- wx-web-rest/src/main/java/com/yuntongxun/controller/TestController.java
+++ wx-web-rest/src/main/java/com/yuntongxun/controller/TestController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -19,7 +16,7 @@ import java.util.concurrent.TimeUnit;
  * @Description: Hello World
  * @Author: lu
  */
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class TestController {
 
@@ -31,7 +28,6 @@ public class TestController {
     private String path;
 
     @RequestMapping("/")
-    @ResponseBody
     public String test() {
         return path;
     }
@@ -39,29 +35,8 @@ public class TestController {
 
     @RequestMapping("/test1")
     public String test1(Map<String, Object> map) {
-        map.put("nameKey", path);
-        return "test";
-    }
-
-    @RequestMapping("/test2")
-    public ModelAndView testView() {
-        ModelAndView mv = new ModelAndView("test");
-        mv.addObject("nameKey", path);
-        mv.addObject("11111", "1111");
-        mv.addObject("11111", "111111");
-        mv.addObject("sssss", "sssssss");
-        mv.addObject("ssscccc", "sssssss");
-        return mv;
-    }
-
-    @RequestMapping("/test3")
-    public ModelAndView test3() {
-        Map<String, String> map = new HashMap();
-        map.put("aaaa", "aaaaa");
-        map.put("bbbb", "bbbbb");
-        map.put("cccc", "你俩的禄口机场斯蒂芬路上慢点");
-        map.put("nameKey", path);
-        return new ModelAndView("test", map);
+        map.put("nameKey111", path);
+        return map.toString();
     }
 
 
@@ -69,7 +44,7 @@ public class TestController {
     public String redis() throws Exception {
         redisTemplate.opsForValue().set("aaa", "ssssss");
         RedisTemplateUtil.set("111111", "wwwww", 1, TimeUnit.MINUTES);
-        return "test";
+        return "cddcdc";
     }
 
 
